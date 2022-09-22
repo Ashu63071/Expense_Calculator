77e8248858810814b93b0917e6a5fcc7bdb16f7e
unknown <cogdotnet975@iiht.tech>
2022-09-22 15:07:23 +0530

    Updates

---
+++ precache-manifest.6049741ed33e36780725b43b39fdeb54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db16307d2dac04eb9a8e",
+    "url": "/Expense_Calculator/static/css/main.735f2720.chunk.css"
+  },
+  {
+    "revision": "db16307d2dac04eb9a8e",
+    "url": "/Expense_Calculator/static/js/main.db16307d.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/Expense_Calculator/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "60ea6223dcd624d3cc0b",
+    "url": "/Expense_Calculator/static/js/2.60ea6223.chunk.js"
+  },
+  {
+    "revision": "dc4760af5acc515e82a0",
+    "url": "/Expense_Calculator/static/js/runtime~main.dc4760af.js"
+  },
+  {
+    "revision": "670cb173734da4458a41450aa41845a8",
+    "url": "/Expense_Calculator/index.html"
+  }
+];
